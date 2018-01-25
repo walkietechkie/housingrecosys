b22f06783175aa67feabea87a786060969ea28d1
mrlmarquez <melissarhonamarquez@gmail.com>
2018-01-25 23:34:13 +0800

    adding mvc app take 2

---
+++ HousingRecommendationSystem/HousingRecommendationSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HousingRecommendationSystem.MvcApplication" Language="C#" %>
---
+++ HousingRecommendationSystem/HousingRecommendationSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HousingRecommendationSystem.Startup))]
+namespace HousingRecommendationSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
