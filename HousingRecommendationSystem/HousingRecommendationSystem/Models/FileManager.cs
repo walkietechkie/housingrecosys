3727af73943774bf3042de3bbb568b9e34e7b0dd
mrlmarquez <melissarhonamarquez@gmail.com>
2018-02-25 15:30:23 +0800

    moved app to another azure acct, removed blob dependency. used google cloud instead.

--- HousingRecommendationSystem/HousingRecommendationSystem/Models/FileManager.cs
+++ HousingRecommendationSystem/HousingRecommendationSystem/Models/FileManager.cs
@@ -1,42 +1,18 @@
-﻿using Microsoft.Azure;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Blob;
-using System;
-using System.Collections.Generic;
+﻿
 using System.IO;
-using System.Linq;
-using System.Web;
 
 namespace HousingRecommendationSystem
 {
     public class FileManager : IFileManager
     {
-        private CloudBlobContainer _container;
-        public FileManager()
-        {
-            Initialize();
-        }
-        private void Initialize()
-        {
-            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
-                                    CloudConfigurationManager.GetSetting("walkietechkie_AzureStorageConnectionString"));
-            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
-
-            // Retrieve a reference to a container.
-            _container = blobClient.GetContainerReference("walkietechkie-container");
-            _container.SetPermissions(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
-        }
+        private string _filePath = "https://storage.googleapis.com/walkietechkie-bucket/ClipsScript.clp";
 
         public string GetClipsFilePath()
         {
-            // Retrieve reference to a blob named "ClipsScript.clp".
-            CloudBlockBlob blockBlob = _container.GetBlockBlobReference("ClipsScript.clp");
-
             var tempPath = Path.GetTempPath() + "\\ClipsScript.clp";
-            // Save blob contents to a file.
-            using (var fileStream = File.OpenWrite(tempPath))
+            using (var wc = new System.Net.WebClient())
             {
-                blockBlob.DownloadToStream(fileStream);
+                wc.DownloadFile(_filePath, tempPath);
             }
 
             return tempPath;
